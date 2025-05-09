28d859b849347f185386650290940148bced98ce
mdalkama <mdalkama689@gmail.com>
2025-05-09 17:08:32 +0530

    add icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import Mobile from "@/components/Mobile";
+import MobileShare from "@/components/share/MobileShare";
+import TabShare from "@/components/share/TabShare";
 import Tab from "@/components/Tab";
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
+  const [deviceType, setDeviceType] = useState<"big" | "small">("big");
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
+      setDeviceType(window.innerWidth < 1024 ? "small" : "big");
     };
 
     handleResize();
@@ -20,5 +24,10 @@ export default function Home() {
     };
   }, []);
 
-  return <div>{isMobile ? <Mobile /> : <Tab />}</div>;
+  return (
+    <div>
+      {isMobile ? <Mobile /> : <Tab />}
+      {deviceType === "small" ? <MobileShare /> : <TabShare />}
+    </div>
+  );
 }
