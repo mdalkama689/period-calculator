935cd1a632b23907d1f225d237c19f53e29de0cf
mdalkama <mdalkama689@gmail.com>
2025-05-10 14:51:32 +0530

    chnge icon

--- src/components/share/TabShare.tsx
+++ src/components/share/TabShare.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
-import { MdOutlineInfo } from "react-icons/md";
+import { IoInformationCircle } from "react-icons/io5";
+
 const TabShare = () => {
   const [isHovered, setIsHovered] = useState(false);
   const whatsappMessage = encodeURIComponent(
@@ -46,7 +47,7 @@ const TabShare = () => {
           </Link>
         </div>
 
-        <MdOutlineInfo
+        <IoInformationCircle
           size={48}
           className="cursor-pointer absolute right-0 transition-transform duration-300 transform hover:scale-110 text-white"
         />
